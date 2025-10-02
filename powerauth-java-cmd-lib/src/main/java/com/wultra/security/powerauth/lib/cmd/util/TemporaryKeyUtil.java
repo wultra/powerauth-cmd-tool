d1742a13a247dace46a3f39b04fe0d5c64e4f245
Roman Strobl <roman.strobl@wultra.com>
2025-10-02 12:33:54 +0800

    Improve logging for failed requests

--- powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/util/TemporaryKeyUtil.java
+++ powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/util/TemporaryKeyUtil.java
@@ -212,8 +212,8 @@ public class TemporaryKeyUtil {
         final ObjectRequest<TemporaryKeyRequest> request = new ObjectRequest<>(jwtData);
         final RestClient restClient = RestClientFactory.getRestClient();
         try {
-            final ObjectResponse<TemporaryKeyResponse> response = Objects.requireNonNull(restClient).postObject(uri, request, null, MapUtil.toMultiValueMap(headers), TemporaryKeyResponse.class);
             stepContext.getStepLogger().writeItem(step.id() + "-temporary-key-request-sent", "Temporary key request sent", "Temporary key request sent to the server", "OK", request);
+            final ObjectResponse<TemporaryKeyResponse> response = Objects.requireNonNull(restClient).postObject(uri, request, null, MapUtil.toMultiValueMap(headers), TemporaryKeyResponse.class);
             stepContext.getStepLogger().writeItem(step.id() + "-temporary-key-fetched", "Temporary key fetched", "Temporary key was fetched from the server", "OK", response);
             handleTemporaryKeyResponse(step, stepContext, response, scope, algorithm);
         } catch (RestClientException ex) {
