2c20219a81d594c8f0dd04a0902563a4d07e0735
Roman Strobl <roman.strobl@wultra.com>
2025-10-02 12:17:11 +0800

    Fix #592: Add request and response logging for temporary keys

--- powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/util/TemporaryKeyUtil.java
+++ powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/util/TemporaryKeyUtil.java
@@ -213,7 +213,8 @@ public class TemporaryKeyUtil {
         final RestClient restClient = RestClientFactory.getRestClient();
         try {
             final ObjectResponse<TemporaryKeyResponse> response = Objects.requireNonNull(restClient).postObject(uri, request, null, MapUtil.toMultiValueMap(headers), TemporaryKeyResponse.class);
-            stepContext.getStepLogger().writeItem(step.id() + "-temporary-key-fetched", "Temporary key fetched", "Temporary key was fetched from the server", "OK", null);
+            stepContext.getStepLogger().writeItem(step.id() + "-temporary-key-request-sent", "Temporary key request sent", "Temporary key request sent to the server", "OK", request);
+            stepContext.getStepLogger().writeItem(step.id() + "-temporary-key-fetched", "Temporary key fetched", "Temporary key was fetched from the server", "OK", response);
             handleTemporaryKeyResponse(step, stepContext, response, scope, algorithm);
         } catch (RestClientException ex) {
             stepContext.getStepLogger().writeServerCallError(step.id() + "-error-server-call", ex.getStatusCode().value(), ex.getResponse(), HttpUtil.flattenHttpHeaders(ex.getResponseHeaders()));
