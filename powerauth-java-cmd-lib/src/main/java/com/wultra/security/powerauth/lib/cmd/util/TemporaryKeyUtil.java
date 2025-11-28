f658e0e3e39788a56a0df929dbf17176ded02c79
Roman Strobl <roman.strobl@wultra.com>
2025-11-28 22:17:14 +0800

    Switch to exceptions for unexpected states

--- powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/util/TemporaryKeyUtil.java
+++ powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/util/TemporaryKeyUtil.java
@@ -346,13 +346,11 @@ public class TemporaryKeyUtil {
     private static void handleSharedSecretResponse(StepContext<? extends BaseStepData, ?> stepContext, JWTClaimsSet claims, SharedSecretAlgorithm algorithm) throws GenericCryptoException {
         final Object claim = claims.getClaim("sharedSecretResponse");
         if (claim == null) {
-            stepContext.getStepLogger().writeError(stepContext.getStep().id() + "-error-missing-shared-secret-response", "Shared secret response is missing");
-            return;
+            throw new IllegalArgumentException("Shared secret response is missing");
         }
         final SharedSecretResponse serverResponse = OBJECT_MAPPER.convertValue(claim, SharedSecretResponse.class);
         if (serverResponse == null) {
-            stepContext.getStepLogger().writeError(stepContext.getStep().id() + "-error-invalid-shared-secret-response", "Shared secret response is invalid");
-            return;
+            throw new IllegalArgumentException("Shared secret response is invalid");
         }
         final SecretKey sharedSecret = SharedSecretUtil.deriveSharedSecret(serverResponse, stepContext.getTemporaryKeyContext().getSharedSecretClientContext(), algorithm);
         stepContext.getTemporaryKeyContext().setTemporarySharedSecret(sharedSecret);
