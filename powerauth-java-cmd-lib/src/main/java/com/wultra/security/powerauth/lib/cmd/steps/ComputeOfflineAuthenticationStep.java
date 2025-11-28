2a089cb06c8f88d42ddc9d104a49cd11f2b601dc
Roman Štrobl <roman.strobl@wultra.com>
2025-11-28 21:40:29 +0700

    Merge pull request #620 from wultra/issues/619-fix-issues-coverity
    Fix #619: Fix issues found by Coverity scan

--- powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/steps/ComputeOfflineAuthenticationStep.java
+++ powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/steps/ComputeOfflineAuthenticationStep.java
@@ -218,6 +218,9 @@ public class ComputeOfflineAuthenticationStep extends AbstractBaseStep<ComputeOf
                     final byte[] kmacData = (offlineDataWithoutSignature.getBytes(StandardCharsets.UTF_8));
 
                     final SecretKey keyMacPersonalisedData = resultStatusObject.getMacPersonalizedDataKeyObject();
+                    if (keyMacPersonalisedData == null) {
+                        throw new IllegalStateException("The macPersonalizedDataKey is missing");
+                    }
 
                     // Construct KMAC-256 tag
                     final byte[] tagKmac = Kmac.kmac256(keyMacPersonalisedData, kmacData, KMAC_OFFLINE_SIGNATURE_CUSTOM_BYTES, 32);
--- powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/util/TemporaryKeyUtil.java
+++ powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/util/TemporaryKeyUtil.java
@@ -170,6 +170,9 @@ public class TemporaryKeyUtil {
                 case APPLICATION_SCOPE -> appSecretBytes;
                 case ACTIVATION_SCOPE -> {
                     final SecretKey transportMasterKey = model.getResultStatus().getTransportMasterKeyObject();
+                    if (transportMasterKey == null) {
+                        throw new IllegalStateException("The transportMasterKey is missing");
+                    }
                     final SecretKey secretKeyBytes = KEY_GENERATOR.deriveSecretKeyHmac(transportMasterKey, appSecretBytes);
                     yield KEY_CONVERTOR.convertSharedSecretKeyToBytes(secretKeyBytes);
                 }
@@ -342,7 +345,13 @@ public class TemporaryKeyUtil {
 
     private static void handleSharedSecretResponse(StepContext<? extends BaseStepData, ?> stepContext, JWTClaimsSet claims, SharedSecretAlgorithm algorithm) throws GenericCryptoException {
         final Object claim = claims.getClaim("sharedSecretResponse");
+        if (claim == null) {
+            throw new IllegalArgumentException("Shared secret response is missing");
+        }
         final SharedSecretResponse serverResponse = OBJECT_MAPPER.convertValue(claim, SharedSecretResponse.class);
+        if (serverResponse == null) {
+            throw new IllegalArgumentException("Shared secret response is invalid");
+        }
         final SecretKey sharedSecret = SharedSecretUtil.deriveSharedSecret(serverResponse, stepContext.getTemporaryKeyContext().getSharedSecretClientContext(), algorithm);
         stepContext.getTemporaryKeyContext().setTemporarySharedSecret(sharedSecret);
         stepContext.getTemporaryKeyContext().setSharedSecretClientContext(null);
