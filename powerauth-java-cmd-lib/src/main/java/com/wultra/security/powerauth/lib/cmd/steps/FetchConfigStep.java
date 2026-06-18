83812b891d17f35a833041e8e18429173293e274
Roman Strobl <roman.strobl@wultra.com>
2026-06-18 14:09:37 +0800

    Add default scope in case it is not specified

--- powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/steps/FetchConfigStep.java
+++ powerauth-java-cmd-lib/src/main/java/com/wultra/security/powerauth/lib/cmd/steps/FetchConfigStep.java
@@ -100,7 +100,7 @@ public class FetchConfigStep extends AbstractBaseStep<FetchConfigStepModel, Encr
         final FetchConfigStepModel model = new FetchConfigStepModel();
         model.fromMap(context);
 
-        final EncryptorScope scope = switch (model.getScope()) {
+        final EncryptorScope scope = switch (model.getScope() != null ? model.getScope() : "application") {
             case "activation" -> EncryptorScope.ACTIVATION_SCOPE;
             case "application" -> EncryptorScope.APPLICATION_SCOPE;
             default -> null;
